072cc69b6d3ba27722b3e1f8b6878dedf2551e2c
Klaus Santos <klausantos82@gmail.com>
2025-02-25 16:45:51 -0300

    Adicionada as classes de security

---
+++ src/main/java/com/k2encoder/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.k2encoder.usuario.infrastructure.repository;
+
+import com.klaus.aprendendospring.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
