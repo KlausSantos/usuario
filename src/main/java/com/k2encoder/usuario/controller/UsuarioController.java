cdcb0cd0c13c48ffb53cf4425a2c81d719985da7
Klaus Santos <klausantos82@gmail.com>
2025-03-07 15:41:44 -0300

    Adicionados métodos GET, DELETE e LOGIN

--- src/main/java/com/k2encoder/usuario/controller/UsuarioController.java
+++ src/main/java/com/k2encoder/usuario/controller/UsuarioController.java
@@ -2,11 +2,14 @@ package com.k2encoder.usuario.controller;
 
 import com.k2encoder.usuario.business.UsuarioService;
 import com.k2encoder.usuario.business.dto.UsuarioDTO;
+import com.k2encoder.usuario.infrastructure.entity.Usuario;
+import com.k2encoder.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-
-import java.security.PublicKey;
 
 @RestController
 @RequestMapping("/usuario")
@@ -14,9 +17,33 @@ import java.security.PublicKey;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
     @PostMapping
     public ResponseEntity<UsuarioDTO> salvaUsuario(@RequestBody UsuarioDTO usuarioDTO) {
         return ResponseEntity.ok(usuarioService.salvaUsuario(usuarioDTO));
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody UsuarioDTO usuarioDTO) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(usuarioDTO.getEmail(),
+                        usuarioDTO.getSenha()
+                )
+        );
+
+        return "Bearer " + jwtUtil.generateToken(authentication.getName());
+    }
+
+    @GetMapping
+    public ResponseEntity<Usuario> buscaUsuarioPorEmail(@RequestParam("email") String email) {
+        return ResponseEntity.ok(usuarioService.buscarUsuarioPorEmail(email));
+    }
+
+    @DeleteMapping("/{email}")
+    public ResponseEntity<Void> deletaUsuarioPorEmail(@PathVariable String email){
+        usuarioService.deletaUsuarioPorEmail(email);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/k2encoder/usuario/infrastructure/repository/EnderecoRepository.java
+++ src/main/java/com/k2encoder/usuario/infrastructure/repository/EnderecoRepository.java
@@ -1,6 +1,6 @@
 package com.k2encoder.usuario.infrastructure.repository;
 
-import com.klaus.aprendendospring.infrastructure.entity.Endereco;
+import com.k2encoder.usuario.infrastructure.entity.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
