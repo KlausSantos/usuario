15e034a80f0458bde1c2cd73011cccca2429cc67
Klaus Santos <klausantos82@gmail.com>
2025-03-08 16:09:03 -0300

    Inclusão de método PUT de usuário.

--- src/main/java/com/k2encoder/usuario/business/UsuarioService.java
+++ src/main/java/com/k2encoder/usuario/business/UsuarioService.java
@@ -6,6 +6,7 @@ import com.k2encoder.usuario.infrastructure.entity.Usuario;
 import com.k2encoder.usuario.infrastructure.exception.ConflictException;
 import com.k2encoder.usuario.infrastructure.exception.ResourceNotFoundException;
 import com.k2encoder.usuario.infrastructure.repository.UsuarioRepository;
+import com.k2encoder.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,7 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO) {
         emailExiste(usuarioDTO.getEmail());
@@ -52,5 +54,17 @@ public class UsuarioService {
 
     public void deletaUsuarioPorEmail(String email) {
         usuarioRepository.deleteByEmail(email);
+    }
+
+    public UsuarioDTO atualizaDadosUsuario(String token, UsuarioDTO dto) {
+        String email = jwtUtil.extrairEmailToken(token.substring(7));
+        dto.setSenha(dto.getSenha() != null ? passwordEncoder.encode(dto.getSenha()) : null);
+        Usuario usuarioEntity = usuarioRepository.findByEmail(email).orElseThrow(() ->
+                new ResourceNotFoundException("Email não localizado")
+        );
+
+        Usuario usuario = usuarioConverter.updateUsuario(dto, usuarioEntity);
+
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
     }
 }
